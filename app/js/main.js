0af686a3238e7aea7756f9c033da9e06cb21e29d
IrinaShm <irinasss2009@yandex.ru>
2020-09-14 00:58:43 +0300

    add part1

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function() {
+
+  $('.featprod__inner').slick({
+    prevArrow:'<button class="slick-arrow slick-prew"><img src="images/icons/slick-left.png" alt="arrow"></button>',
+    nextArrow:'<button class="slick-arrow slick-next"><img src="images/icons/slick-right.png" alt="arrow"></button>',
+  });
   
+  $(".rate-star").rateYo({
+    rating: 4.5,
+    starWidth: "15px",
+    readOnly: true,
+    normalFill: "#fff",
+  });
+
+  var mixer = mixitup('.relprod__inner-box');
+
 });
+
