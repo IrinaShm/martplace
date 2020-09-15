753f89cbeae51c4774a40d2f810bb1a0279503c1
IrinaShm <irinasss2009@yandex.ru>
2020-09-15 14:24:28 +0300

    add any

--- app/js/main.js
+++ app/js/main.js
@@ -14,5 +14,14 @@ $(function() {
 
   var mixer = mixitup('.relprod__inner-box');
 
+  $('.yourprod__inner').slick({
+    // infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+    prevArrow:'<button class="slick-arrow slick-prew prew-color"><img src="images/icons/slick-left.png" alt="arrow"></button>',
+    nextArrow:'<button class="slick-arrow slick-next next-color"><img src="images/icons/slick-right.png" alt="arrow"></button>',
+  });
+
 });
 
